0da6c05797e79f497fa8fd1063ca85ed3b4df211
Bony Yamin <bonyyamin1997@gmail.com>
2025-03-14 00:51:43 +0600

    Initial project structure and configuration files

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const primary = Color(0xFFC1FD50); // Bright green
+  static const secondary = Color(0xFFFD6333); // Orange
+  static const background = Color(0xFFE1E5EA); // Light gray
+}
+
---
+++ lib/core/services/auth_service.dart
@@ -0,0 +1,1 @@
+//firebase or auth logic
